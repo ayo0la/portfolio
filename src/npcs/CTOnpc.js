1a001f940ac8791571be38c468628616e5d7a28f
ayo0la <168016617+ayo0la@users.noreply.github.com>
2026-03-17 23:48:29 -0400

    feat: add BuilderNPC, update dialogue bank, remove 3 unused NPCs

--- src/npcs/CTOnpc.js
+++
@@ -1,9 +0,0 @@
-import { NPCBase } from './NPCBase.js'
-import { NPCS } from '../utils/Constants.js'
-
-export class CTOnpc extends NPCBase {
-  constructor() {
-    const d = NPCS.cto
-    super({ ...d, emoji: '⚙️' })
-  }
-}
