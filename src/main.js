7388208eac7a5cbc6c84f841f5dd3f8b708ce4d3
ayo0la <168016617+ayo0la@users.noreply.github.com>
2026-03-18 12:42:40 -0400

    feat: add background canvas with grain and particle system

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 // src/main.js
 import './style.css'
-console.log('portfolio init')
+import { initBackgroundCanvas, updateBackgroundCanvas } from './canvas/BackgroundCanvas.js'
+
+initBackgroundCanvas()
+
+function loop(ts) {
+  updateBackgroundCanvas(ts)
+  requestAnimationFrame(loop)
+}
+requestAnimationFrame(loop)
