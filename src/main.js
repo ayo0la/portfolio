0aa92ace57bfbc3d5e2ad4bd248bffba86cb2728
ayo0la <168016617+ayo0la@users.noreply.github.com>
2026-03-18 12:55:15 -0400

    feat: add section JS modules matching spec file structure

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 // src/main.js
 import './style.css'
 import { initBackgroundCanvas, updateBackgroundCanvas } from './canvas/BackgroundCanvas.js'
-import { initCursor } from './cursor/Cursor.js'
+import { initCursor }       from './cursor/Cursor.js'
 import { initSmoothScroll } from './scroll/SmoothScroll.js'
-import { initAnimations } from './scroll/Animations.js'
+import { initAnimations }   from './scroll/Animations.js'
+import { initHero }         from './sections/hero.js'
+import { initAbout }        from './sections/about.js'
+import { initMdfld }        from './sections/mdfld.js'
+import { initProjects }     from './sections/projects.js'
+import { initContact }      from './sections/contact.js'
 
 initBackgroundCanvas()
 initCursor()
 initSmoothScroll()
 initAnimations()
+initHero()
+initAbout()
+initMdfld()
+initProjects()
+initContact()
 
 function loop(ts) {
   updateBackgroundCanvas(ts)
---
+++ src/sections/contact.js
@@ -0,0 +1,2 @@
+// src/sections/contact.js
+export function initContact() {}
