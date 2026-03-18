8e89a1e00bcf609e74bd3f8e42d4554d0a62ff70
ayo0la <168016617+ayo0la@users.noreply.github.com>
2026-03-18 12:51:11 -0400

    feat: add Lenis smooth scroll and GSAP scroll animations with word/line stagger

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 import './style.css'
 import { initBackgroundCanvas, updateBackgroundCanvas } from './canvas/BackgroundCanvas.js'
 import { initCursor } from './cursor/Cursor.js'
+import { initSmoothScroll } from './scroll/SmoothScroll.js'
+import { initAnimations } from './scroll/Animations.js'
 
 initBackgroundCanvas()
 initCursor()
+initSmoothScroll()
+initAnimations()
 
 function loop(ts) {
   updateBackgroundCanvas(ts)
---
+++ src/scroll/SmoothScroll.js
@@ -0,0 +1,14 @@
+// src/scroll/SmoothScroll.js
+import Lenis from 'lenis'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export function initSmoothScroll() {
+  const lenis = new Lenis()
+  lenis.on('scroll', ScrollTrigger.update)
+  gsap.ticker.add((time) => lenis.raf(time * 1000))
+  gsap.ticker.lagSmoothing(0)
+  return lenis
+}
