f375fc4e6d01047c454dec2a25bedcf959f029aa
ayo0la <168016617+ayo0la@users.noreply.github.com>
2026-03-18 12:44:18 -0400

    feat: add custom circle cursor with hover and 100ms click flash

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // src/main.js
 import './style.css'
 import { initBackgroundCanvas, updateBackgroundCanvas } from './canvas/BackgroundCanvas.js'
+import { initCursor } from './cursor/Cursor.js'
 
 initBackgroundCanvas()
+initCursor()
 
 function loop(ts) {
   updateBackgroundCanvas(ts)
