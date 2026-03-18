21f6810fbc4d03e2a80d9436894ee8c97607dba2
ayo0la <168016617+ayo0la@users.noreply.github.com>
2026-03-18 12:34:35 -0400

    chore: remove old stadium code, install gsap and lenis

--- src/npcs/AyoolaNPC.js
+++
@@ -1,9 +0,0 @@
-import { NPCBase } from './NPCBase.js'
-import { NPCS } from '../utils/Constants.js'
-
-export class AyoolaNPC extends NPCBase {
-  constructor() {
-    const d = NPCS.ayoola
-    super({ ...d, emoji: '🧠' })
-  }
-}
--- src/npcs/BuilderNPC.js
+++
@@ -1,9 +0,0 @@
-import { NPCBase } from './NPCBase.js'
-import { NPCS } from '../utils/Constants.js'
-
-export class BuilderNPC extends NPCBase {
-  constructor() {
-    const d = NPCS.builder
-    super({ ...d, emoji: '⚙️' })
-  }
-}
